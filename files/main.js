11f10686f396ee7101130d74f09a78ead1511831
skyElmax <sky.elmax@yandex.ru>
2021-08-13 15:28:43 +0300

    finish him

---
+++ files/main.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            notifi: [],
+        }
+    },
+    mounted() {
+        window.addN = this.createNotifi;
+    },
+    methods: {
+        createNotifi(type, time, message, title = "Уведомление") {
+            let n = this.notifi.find(_el => _el.message === message);
+            if (n) return n.gsap.restart();
+            let notifi = this.notifi[this.notifi.push({type, title, message, loading: 100}) - 1];
+            notifi.gsap = gsap.to(notifi, {
+                duration: (time / 1000) + 0.5,
+                loading: 0,
+                onComplete: () => {
+                    let ind = this.notifi.indexOf(notifi);
+                    if (ind !== -1) this.notifi.splice(ind, 1);
+                }
+            })
+        }
+    },
+    computed: {
+    },
+    watch: {
+    }
+});
